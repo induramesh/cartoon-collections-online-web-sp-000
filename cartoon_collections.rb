06b26bd03c6a1623bacb3b4e70c6e18146a33faa
Indu Ramesh <induramesh@gmail.com>
2019-07-22 20:24:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheese_types.map {|cheese| arr.include?(cheese)}
-    cheese
+    
   else 
     nil 
   end 
